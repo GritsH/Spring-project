0806b5a5fa5b1a31dff4cb2520a824ab93c1c0b8
GritsH <grits.lena@gmail.com>
2022-09-13 20:54:07 +0300

    add log out

---
+++ src/main/java/by/grits/newsSpring/controller/LogOutController.java
@@ -0,0 +1,18 @@
+package by.grits.newsSpring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LogOutController {
+    @GetMapping("/logout")
+    public String logOut(HttpSession session) {
+        if (session.getAttribute("current_user") != null) {
+            session.removeAttribute("current_user");
+        }
+        return "redirect:/";
+    }
+}
