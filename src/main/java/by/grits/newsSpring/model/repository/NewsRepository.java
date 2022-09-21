4e090d493563706aeba39c65945251ee5fed7919
GritsH <grits.lena@gmail.com>
2022-09-21 09:41:06 +0300

    minor fixes

--- src/main/java/by/grits/newsSpring/model/repository/NewsRepository.java
+++ src/main/java/by/grits/newsSpring/model/repository/NewsRepository.java
@@ -1,7 +1,6 @@
 package by.grits.newsSpring.model.repository;
 
 import by.grits.newsSpring.model.News;
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,14 +11,9 @@ import java.util.List;
 @Repository
 public interface NewsRepository extends CrudRepository<News, Long> {
     News save(News news);
-
-    @Modifying
-    @Query("delete from news where id=?")
-    void deleteById(@Param("id") Integer id);
-
+    
     @Query("select id, title, summary, content, author, added_at from news where id=:id")
     News getById(@Param("id") Long id);
 
-    @Query("select id, title, summary, content, author, added_at from news")
     List<News> findAll();
 }
