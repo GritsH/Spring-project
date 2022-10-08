a615d53d498ef971bf02546f4c3994d217f990f6
GritsH <grits.lena@gmail.com>
2022-10-08 11:32:38 +0300

    optimized imports

--- src/main/java/by/grits/newsSpring/model/repository/NewsRepository.java
+++ src/main/java/by/grits/newsSpring/model/repository/NewsRepository.java
@@ -1,12 +1,8 @@
 package by.grits.newsSpring.model.repository;
 
 import by.grits.newsSpring.model.News;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface NewsRepository extends CrudRepository<News, Long> {
--- src/main/java/by/grits/newsSpring/model/repository/UserRepository.java
+++ src/main/java/by/grits/newsSpring/model/repository/UserRepository.java
@@ -1,15 +1,10 @@
 package by.grits.newsSpring.model.repository;
 
 import by.grits.newsSpring.model.User;
-import org.springframework.data.jdbc.repository.query.Modifying;
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
-    @Query("select email,user_password, role_type, added_at from user where email=:email_address")
-    User findByEmail(@Param("email_address") String email);
 
 }
